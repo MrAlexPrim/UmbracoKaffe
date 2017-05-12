eb68e5cbbc785e908ec4b6052d3e96957f4e6ebe
Alexander Prim <alexprim@gmail.com>
2017-05-12 15:02:35 +0200

    bytt namn. Hör ihop med förra

--- KaffePTCloud.Core/Controllers/SiteLayoutController.cs
+++ KaffePTCloud.Core/Controllers/SiteLayoutController.cs
@@ -6,7 +6,7 @@ namespace KaffePTCloud.Core.Controllers
     public class SiteLayoutController : SurfaceController
     {
         public ActionResult _Head() => PartialView("~/Views/Partials/SiteLayout/_Head.cshtml");
-        public ActionResult _Header() => PartialView("~/Views/Partials/SiteLayout/_Hegader.cshtml");
+        public ActionResult _Hegader() => PartialView("~/Views/Partials/SiteLayout/_Hegader.cshtml");
         public ActionResult _Footer() => PartialView("~/Views/Partials/SiteLayout/_Footer.cshtml");
         public ActionResult _Scripts() => PartialView("~/Views/Partials/SiteLayout/_Scripts.cshtml");
 
